05fe5ccf644674e6f42bcec581487507fd94afb8
bill <williamandrewbain@gmail.com>
2018-12-12 11:20:40 -0700

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,57 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
+import Analysis from "./components/Analysis.js";
 
 class App extends Component {
+  state = {
+    content: "",
+    toneAnalysis: undefined,
+    topTones: undefined
+  };
+
+  tonePost = () => {
+    if (!this.state.content.length) {
+      alert("bogus input");
+    }
+    fetch(
+      "https://gateway.watsonplatform.net/tone-analyzer/api/v3/tone?version=2017-09-21",
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          "cache-control": "no-cache",
+          Authorization:
+            "Basic YXBpa2V5Ojk2ZkM4U1lSdmFfTDFwckpHWE1QRkc2enRkeDZvel9mMExJSWNJMi16eDdO"
+        },
+        body: JSON.stringify({
+          text: this.state.content
+        })
+      }
+    )
+      .then(response => response.json())
+      .then(json => this.setState({ toneAnalysis: json }))
+      .then(this.topTones);
+  };
+
+  topTones = () => {
+    let unsortedTones = this.state.toneAnalysis.document_tone.tones;
+    console.log(
+      unsortedTones.sort(function(a, b) {
+        return b.score - a.score;
+      })
+    );
+  };
+
+  contentListener = event => {
+    this.setState({ content: event.target.value });
+  };
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div>
+        <h3>MindReader</h3>
+        <textarea onChange={this.contentListener} value={this.state.content} />
+        <button onClick={this.tonePost}>Read minds</button>
       </div>
     );
   }
---
+++ src/components/Analysis.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Analysis = ({ analysis }) => {
+
+
+  return (
+    <div>
+      <p>Whassup</p>
+      <button>What're the top tones?</button>
+    </div>
+  );
+};
+
+export default Analysis;
