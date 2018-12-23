db60f287311031523a64698bafc6b866ddc8e1c1
bill <williamandrewbain@gmail.com>
2018-12-22 19:48:42 -0700

    updated chart coloring

--- src/components/Analysis.js
+++ src/components/Analysis.js
@@ -32,7 +32,7 @@ class Analysis extends Component {
               tone => tone.score
             ),
             backgroundColor: [
-              "rgba(255, 99, 132, 0.2)",
+              "rgba(255, 99, 131, 0.2)",
               "rgba(54, 162, 235, 0.2)",
               "rgba(255, 206, 86, 0.2)",
               "rgba(75, 192, 192, 0.2)",
